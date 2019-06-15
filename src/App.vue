0628291a0ff5785ffa6e1042fe48d6f57fbeb095
unknown <mark_o002@hotmail.com>
2019-06-15 16:05:43 -0500

    integracion de bootstrap 3.3.7

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
+
+  <nav class="navbar navbar-default" id="nav">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">SPA Vue</a>
+      </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li>
+            <a><router-link to="/">Home</router-link></a>
+          </li>
+          <li>
+            <a><router-link to="/about">About</router-link></a>
+          </li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+
+
+    <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+    </div>-->
+
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
+
   </div>
 </template>
 
 <style>
+/*@import 'src/assets/bootstrap/css/bootstrap.min.css';*/
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,9 +49,9 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
+/*#nav {
   padding: 30px;
-}
+}*/
 
 #nav a {
   font-weight: bold;
