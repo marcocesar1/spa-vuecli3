7113ee3646b19f0359d21d58c185d2f10f0ed5a1
unknown <mark_o002@hotmail.com>
2019-06-15 13:41:02 -0500

    prueba compatibilidad ie

--- src/store.js
+++ src/store.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    persona: []
   },
   mutations: {
-
+    setPersona(state, persona){
+      state.persona = persona;
+    }
   },
   actions: {
-
+    getPersona(context){
+      Axios.get('http://jsonplaceholder.typicode.com/todos/1').then( response => {        
+        context.commit('setPersona', response.data)
+      })
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <p><strong>{{persona}}</strong></p>
+    <small>data desde api test - run ie</small>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +10,15 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import {mapState} from 'vuex'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  computed: {
+    ...mapState(['persona'])
   }
 }
 </script>
